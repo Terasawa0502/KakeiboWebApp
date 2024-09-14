6be1569302ce68769d1780f167b8d8d8a579539b
寺澤徳隆 <mosqey1889@gmail.com>
2024-09-14 20:16:26 +0900

    経費データ読み取り機能の実装

--- WEB-INF/src/data/ExpensesDao.java
+++ WEB-INF/src/data/ExpensesDao.java
@@ -1,5 +1,89 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import util.Constants;
+
+
+// 経費データ操作用DAOクラス
 public class ExpensesDao {
 
+	//データの読み取りメソッド
+	public ArrayList<ExpensesDto> select(int id, String keyword, String order) {
+		// 取得したデータを格納するためのリスト
+		ArrayList<ExpensesDto> expensesDataList = new ArrayList<>();
+		
+		// 文字列がNULLだった場合は空文字に置き換え
+		keyword = Objects.toString(keyword, "");
+		order = Objects.toString(order, "");
+		
+		// SELECT用のSQL
+		String sql = "SELECT * FROM expenses ";
+		
+		// 引数に合わせてSELECT文に条件を追加
+		if (id > 0) {
+			// IDが指定があった場合(データ編集時)
+			sql += "WHERE id = ?;";
+		} else {
+			// 名前で検索キーワードがあった場合(データ検索時)
+			if (!keyword.isEmpty()) {
+				sql += "WHERE name LIKE ?";
+			}
+			// 並べ替え方向に合わせてORDER BY句を追加
+			// desc:降順 asc・指定なし:昇順
+			sql += (order.equals("desc")) ? "ORDER BY date DESC" : "ORDER BY date ASC";
+			// 文末にセミコロンを追加
+			sql += ";";
+		}
+		// SQL文の送信準備
+		try (Connection connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD);
+				PreparedStatement statement = connection.prepareStatement(sql))
+		{
+			// ?があれば置き換え
+			if (id > 0) {
+				// IDが指定されていた場合置き換え
+				statement.setInt(1, id);
+			} else if (!keyword.isEmpty()) {
+				// キーワードが指定されていた場合置き換え
+				statement.setString(1, "%" + keyword + "%");
+			}
+			// SQL文を実行
+			ResultSet resultSet = statement.executeQuery();
+			
+			// SQLクエリの実行結果を抽出
+			while (resultSet.next()) {
+				// DTOのインスタンスに各カラムのデータをセット
+				ExpensesDto expensesData = new ExpensesDto();
+				expensesData.setId(resultSet.getInt("id"));
+				expensesData.setName(resultSet.getString("name"));
+				expensesData.setPrice(resultSet.getInt("price"));
+				expensesData.setCategory_id(resultSet.getInt("category_id"));
+				expensesData.setMemo(resultSet.getString("memo"));
+				
+				// Date型→LocalDate型へ変換
+				Date sqlDate = resultSet.getDate("date");
+				if (sqlDate != null) {
+					LocalDate localDate = sqlDate.toLocalDate();
+					expensesData.setDate(localDate);
+				}
+				// リストにデータを追加
+				expensesDataList.add(expensesData);
+			}
+		} catch (SQLException e) {
+			System.out.println(Constants.DE001 + e.getMessage());
+		}
+		
+		return expensesDataList;
+		
+	}
+	
+	
 }
--- WEB-INF/src/util/Constants.java
+++ WEB-INF/src/util/Constants.java
@@ -1,5 +1,18 @@
 package util;
 
+// 共通定数を定義するクラス
 public class Constants {
 
+	/*
+	 * DB接続情報
+	 */
+	public static final String URL = "jdbc:mariadb://localhost:8889/java_db_app_kakeibo"; //URL
+	public static final String USER = "root"; //ユーザ名
+	public static final String PASSWORD = "root"; //パスワード
+	
+	/*
+	 * エラーメッセージ
+	 */
+	// データベース関連
+	public static final String DE001 = "経費データ読み取り失敗：";
 }
