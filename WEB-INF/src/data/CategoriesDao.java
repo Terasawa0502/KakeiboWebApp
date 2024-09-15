1f864d9fbd116e74d05223c4b0d4b03b176c0715
寺澤徳隆 <mosqey1889@gmail.com>
2024-09-15 16:22:54 +0900

    カテゴリーデータ読み取り機能の修正

--- WEB-INF/src/data/CategoriesDao.java
+++ WEB-INF/src/data/CategoriesDao.java
@@ -2,9 +2,9 @@ package data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import util.Constants;
@@ -13,17 +13,29 @@ import util.Constants;
 public class CategoriesDao {
 
 	// カテゴリーデータの読み取り
-	public ArrayList<CategoriesDto> select() throws SQLException {
+	public ArrayList<CategoriesDto> select(int categoryId) {
 		// 取得したデータを格納するためのリスト
 		ArrayList<CategoriesDto> categoriesDataList = new ArrayList<>();
 		// SELECT文のフォーマット
-		String sql = "SELECT * FROM categories";
+		String sql = "SELECT * FROM categories ";
+		// 引数に合わせてSELECT文に条件を追加
+		if (categoryId > 0) {
+			// IDに指定があった場合(データ一覧表示時)
+			sql += "WHERE category_id = ?";
+		}
+		// 文末にセミコロンを追加
+		sql += ";";
+		
 		// データベースへの接続・SQL文の送信準備
 		try(Connection connection = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD);
-			Statement statement = connection.createStatement()) 
+			PreparedStatement statement = connection.prepareStatement(sql)) 
 		{
+			// ?があれば置き換え
+			if (categoryId > 0) {
+				statement.setInt(1, categoryId);
+			}
 			// SQL文を実行
-			ResultSet resultSet = statement.executeQuery(sql);
+			ResultSet resultSet = statement.executeQuery();
 			// SQLクエリの実行結果を抽出
 			while (resultSet.next()) {
 				// DTOのインスタンスにデータをセット
@@ -33,6 +45,8 @@ public class CategoriesDao {
 				// リストにデータを追加
 				categoriesDataList.add(categoriesData);
 			}
+		} catch (SQLException e) {
+			System.out.println(Constants.DE005 + e.getMessage());
 		}
 		// カテゴリーデータリストを返す
 		return categoriesDataList;
