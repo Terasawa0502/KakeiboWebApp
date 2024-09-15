6b50c634571ac9ab9706d249e6fcdaf796075205
寺澤徳隆 <mosqey1889@gmail.com>
2024-09-15 16:25:23 +0900

    一覧ページ表示サーブレットの実装

--- WEB-INF/src/servlet/ListServlet.java
+++ WEB-INF/src/servlet/ListServlet.java
@@ -1,7 +1,45 @@
 package servlet;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import data.ExpensesDao;
+import data.ExpensesDto;
+import util.Constants;
+
+// 経費データ一覧ページ用Servlet
 public class ListServlet extends HttpServlet {
-
+	// GETメソッドのリクエスト受信時に実行されるメソッド
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // リクエスト・レスポンスの設定
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		// 経費データリストのインスタンスを生成
+		ArrayList<ExpensesDto> expensesDataList = new ArrayList<>();
+		// 経費データ操作用DAOのインスタンス生成
+		ExpensesDao expensesData = new ExpensesDao();
+		try {
+			// 経費データの一覧を取得(ID指定・並べ替え・キーワードなし)
+			expensesDataList = expensesData.select(0, "", "");
+			if (expensesDataList.isEmpty()) {
+				// 経費データリストが空だった場合は失敗メッセージを保存
+				req.setAttribute(Constants.FAILURE_MESSAGE, Constants.NODATA_EXPENSES_MESSAGE);
+			} else {
+				// 経費データリストが空じゃない場合は経費データリストを保存
+				req.setAttribute(Constants.EXPENSES_DATA_LIST, expensesDataList);
+			}
+		} catch (Exception e) {
+			// データベース処理の例外発生時
+			req.setAttribute(Constants.FAILURE_MESSAGE, Constants.DB_EXCEPTION_MESSAGE);
+		}
+		// フォワードによる画面遷移
+		req.getRequestDispatcher(Constants.LIST_PAGE_JSP).forward(req, resp);
+	}
 }
--- WEB-INF/src/util/Constants.java
+++ WEB-INF/src/util/Constants.java
@@ -6,16 +6,34 @@ public class Constants {
 	/*
 	 * DB接続情報
 	 */
-	public static final String URL = "jdbc:mariadb://localhost:8889/java_db_app_kakeibo"; //URL
-	public static final String USER = "root"; //ユーザ名
-	public static final String PASSWORD = "root"; //パスワード
+	public static final String URL = "jdbc:mariadb://localhost:8889/java_db_app_kakeibo";
+	public static final String USER = "root";
+	public static final String PASSWORD = "root";
 	
 	/*
-	 * エラーメッセージ
+	 * 経費データ関連メッセージ
 	 */
-	// 経費データ関連
+	// エラーメッセージ
 	public static final String DE001 = "経費データ読み取り失敗：";
 	public static final String DE002 = "経費データ追加失敗：";
 	public static final String DE003 = "経費データ更新失敗：";
 	public static final String DE004 = "経費データ削除失敗：";
+	
+	/*
+	 * カテゴリーデータ関連メッセージ
+	 */
+	public static final String DE005 = "カテゴリーデータ読み取り失敗：";
+	
+	/*
+	 * Servlet関連メッセージ
+	 */
+	// 共通エラーメッセージ
+	public static final String NODATA_EXPENSES_MESSAGE = "該当するデータが見つかりませんでした。";
+	public static final String DB_EXCEPTION_MESSAGE = "データベース処理エラーが発生しました。システム管理者に確認してください。";
+	//  共通部品
+	public static final String FAILURE_MESSAGE = "failureMessage";
+	public static final String SUCCESS_MESSAGE = "sucessMessage";
+	public static final String EXPENSES_DATA_LIST = "expensesDataList";
+	public static final String LIST_PAGE_JSP = "/WEB-INF/jsp/listPage.jsp";
+	
 }
