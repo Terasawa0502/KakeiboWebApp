bafc96c8aace52a956eb9a49e6c7c61b2469c068
寺澤徳隆 <mosqey1889@gmail.com>
2024-09-16 15:46:10 +0900

    データ登録機能の実装

--- WEB-INF/src/util/Constants.java
+++ WEB-INF/src/util/Constants.java
@@ -31,11 +31,16 @@ public class Constants {
 	public static final String NODATA_EXPENSES_MESSAGE = "該当するデータが見つかりませんでした。";
 	public static final String NODATA_CATEGORIES_MESSAGE = "カテゴリーデータが1件も登録されていません。";
 	public static final String DB_EXCEPTION_MESSAGE = "データベース処理エラーが発生しました。システム管理者に確認してください。";
+	public static final String PRICE_COVRESION_MESSAGE = "数値の入力形式に誤りがあります。有効な数値を入力してください。";
+	public static final String DATE_COVRESION_MESSAGE = "日付の変換処理失敗が発生しました。有効な日付を入力してください。";
+	public static final String NAME_MISS_MESSAGE = "有効な購入品名を入力してください。";
+	public static final String REGISTER_DATA_MESSAGE = "件のデータを登録しました。";
 	//  共通部品
 	public static final String FAILURE_MESSAGE = "failureMessage";
 	public static final String SUCCESS_MESSAGE = "sucessMessage";
 	public static final String EXPENSES_DATA_LIST = "expensesDataList";
 	public static final String CATEGORIES_DATA_LIST = "categoriesDataList";
+	public static final String LIST_SERVLET_PAGE = "/list";
 	public static final String LIST_PAGE_JSP = "/WEB-INF/jsp/listPage.jsp";
 	public static final String REGISTER_PAGE_JSP = "/WEB-INF/jsp/registerPage.jsp";
 	
